ba9433a79037c2a2325897fac6102897dbfe8f6c
Morgan Sun <64807003+sunmorgan@users.noreply.github.com>
2023-02-10 11:37:11 -0500

    update on socket route

--- WebSocketRoute.h
+++ WebSocketRoute.h
@@ -1,16 +1,19 @@
 #ifndef WEBSOCKETROUTE_H
 #define WEBSOCKETROUTE_H
 
+#include "Route.h"
+
 #include <unordered_map>
 #include <string>
 
 struct MHD_Connection;
 
-class WebSocketRoute
+class WebSocketRoute : public Route
 {
 public:
     virtual ~WebSocketRoute() {}
 
+    virtual void handleWebSocketRequest(int socket) = 0;
     virtual void onConnect(MHD_Connection *connection) = 0;
     virtual void onMessage(MHD_Connection *connection, const std::string &message) = 0;
     virtual void onClose(MHD_Connection *connection, enum MHD_RequestTerminationCode toe) = 0;
